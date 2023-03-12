49bddfed986c760693c65ff4fe57c264c78354ac
Sergio Tapia <stapias@uninorte.edu.co>
2023-03-11 21:21:57 -0500

    Contact Form update

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+const formulariodata = document.getElementById("botonForm");
+
+formulariodata.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const Nombre = document.getElementById("Nombre").value;
+  const correoelectronico = document.getElementById("correoelectronico").value;
+  const telefono = document.getElementById("telefono").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  object = {
+    senderName: Nombre,
+    senderEmail: correoelectronico,
+    senderPhone: telefono,
+    message: mensaje,
+  };
+
+  json = JSON.stringify(object);
+
+  fetch(
+    "https://be47q9xtx3.execute-api.us-east-1.amazonaws.com/default/emailHander",
+    {
+      method: "POST",
+      body: json,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+});
